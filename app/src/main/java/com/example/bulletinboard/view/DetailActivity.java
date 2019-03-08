22f285022b93ae06d8dd58f2802742fb0ef423e1
Susan Kim <suzenyada@gmail.com>
2019-03-08 17:54:24 +0900

    Show detail view

--- app/src/main/java/com/example/bulletinboard/view/DetailActivity.java
+++ app/src/main/java/com/example/bulletinboard/view/DetailActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.bulletinboard.R;
+import com.example.bulletinboard.model.Post;
 
 public class DetailActivity extends AppCompatActivity {
 
     Toolbar actionBar;
+
+    TextView title_tv;
+    TextView content_tv;
+
+    private Post post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +28,25 @@ public class DetailActivity extends AppCompatActivity {
         setSupportActionBar(actionBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        title_tv = findViewById(R.id.detail_title_tv);
+        content_tv = findViewById(R.id.detail_content_tv);
+
+        Intent intent = getIntent();
+
+        int number = intent.getIntExtra("number", 0);
+        String title = intent.getStringExtra("title");
+        String content = intent.getStringExtra("content");
+
+        post = new Post(number, title, content);
+
+        title_tv.setText(post.title);
+        content_tv.setText(post.content);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         navigateUpTo(new Intent(this, MainActivity.class));
-        
+
         return true;
     }
 
