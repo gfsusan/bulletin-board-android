72623215a06b5de88cacd6e165e5c4d90d1c4722
Susan Kim <suzenyada@gmail.com>
2019-03-08 18:18:43 +0900

    Update add acvitivy ui

--- app/src/main/java/com/example/bulletinboard/view/AddActivity.java
+++ app/src/main/java/com/example/bulletinboard/view/AddActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.bulletinboard.R;
 
 public class AddActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+
+    EditText title_tf;
+    EditText content_tf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,11 @@ public class AddActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.add_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+
+        title_tf = findViewById(R.id.title_tf);
+        content_tf = findViewById(R.id.content_tf);
+
     }
 
 
