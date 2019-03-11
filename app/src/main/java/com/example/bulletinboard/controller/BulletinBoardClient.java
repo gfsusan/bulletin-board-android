bcf9b81fa2bbfac4fa7ba462b5b6bb345b54bc23
Susan Kim <suzenyada@gmail.com>
2019-03-11 17:04:31 +0900

    Adding post implemented

--- app/src/main/java/com/example/bulletinboard/controller/BulletinBoardClient.java
+++ app/src/main/java/com/example/bulletinboard/controller/BulletinBoardClient.java
@@ -3,6 +3,8 @@ package com.example.bulletinboard.controller;
 import android.content.Context;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -16,6 +18,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BulletinBoardClient {
     private static final String TAG = "BulletinBoardClient";
@@ -55,6 +59,10 @@ public class BulletinBoardClient {
                 for (JsonElement jsonElement : jsonArray) {
                     JsonObject jo = (JsonObject) jsonElement;
 
+                    // number 값이 null이면 skip
+                    if (jo.get("number").isJsonNull()) {
+                        continue;
+                    }
                     int number = jo.get("number").getAsInt();
                     String title = jo.get("title").getAsString();
                     String content = jo.get("content").getAsString();
@@ -72,6 +80,46 @@ public class BulletinBoardClient {
                 callback.onFailure();
             }
         });
+
+        stringRequest.setTag(TAG);
+        queue.add(stringRequest);
+    }
+
+    public void addPost(final int number, final String title, final String content, final VolleyCallBack callBack) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, baseURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                callBack.onSuccess();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                callBack.onError(new Throwable());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("number", "" + number);
+                params.put("title", title);
+                params.put("content", content);
+
+                Log.i(TAG, "number : " + number);
+                Log.i(TAG, "title : " + title);
+                Log.i(TAG, "content : " + content);
+
+                return params;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                int statusCode = response.statusCode;
+                if (statusCode == 400) {
+                    callBack.onError(new Throwable());
+                }
+                return super.parseNetworkResponse(response);
+            }
+        };
 
         stringRequest.setTag(TAG);
         queue.add(stringRequest);
--- app/src/main/java/com/example/bulletinboard/view/AddActivity.java
+++ app/src/main/java/com/example/bulletinboard/view/AddActivity.java
@@ -1,25 +1,42 @@
 package com.example.bulletinboard.view;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.bulletinboard.R;
+import com.example.bulletinboard.controller.BulletinBoardClient;
+import com.example.bulletinboard.controller.VolleyCallBack;
 
 public class AddActivity extends AppCompatActivity {
+
+    private final String TAG = "AddActivity";
+    Context context;
 
     Toolbar toolbar;
 
     EditText title_tf;
     EditText content_tf;
 
+    BulletinBoardClient bbc;
+
+    // number for keeping track of "number" field
+    int number = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        context = this;
 
         toolbar = findViewById(R.id.add_toolbar);
         setSupportActionBar(toolbar);
@@ -29,12 +46,49 @@ public class AddActivity extends AppCompatActivity {
         title_tf = findViewById(R.id.title_tf);
         content_tf = findViewById(R.id.content_tf);
 
+        bbc = BulletinBoardClient.getInstance(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_menu, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        navigateUpTo(new Intent(this, MainActivity.class));
+        switch (item.getItemId()) {
+            case R.id.add_action_save:
+                final String title = title_tf.getText().toString();
+                final String content = content_tf.getText().toString();
+
+                VolleyCallBack callBack = new VolleyCallBack() {
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.i(TAG, "BAD_REQUEST");
+                        number ++;
+                        bbc.addPost(number, title, content, this);
+                        Log.i(TAG, "number : " + number);
+                    }
+
+                    @Override
+                    public void onSuccess() {
+                        navigateUpTo(new Intent(context, MainActivity.class));
+                        number++;
+                    }
+
+                    @Override
+                    public void onFailure() {
+
+                    }
+                };
+
+                bbc.addPost(number, title, content, callBack);
+                return true;
+            default:
+                navigateUpTo(new Intent(this, MainActivity.class));
+        }
 
         return true;
     }
